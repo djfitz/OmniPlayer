d7f582411e43d122c86b18650c1ea9b56631a537
Doug Hill <doug.hill@udemy.com>
2018-09-16 11:50:14 -0700

    Initial Commit

---
+++ MultiVideoStreaming/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiVideoStreaming
+//
+//  Created by Doug Hill on 9/16/18.
+//  Copyright © 2018 doughill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
