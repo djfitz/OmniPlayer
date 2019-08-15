12bd59f17e64c9693bb8f0da1bd7136c7a86d8a1
Doug Hill <doug@breaqz.com>
2019-08-15 16:33:51 -0700

    Finished the standalone demo app so that it has minimal functionality; e.g. can play a movie stream. This app embeds the MultiPlayer framework and defines no view controller; it just creates a Multipler VC as the root VC of the app. I then tell the media player manager to load a movie, and the VC shows the movie and the player controls.

---
+++ PlayerDemoApp/PlayerDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayerDemoApp
+//
+//  Created by Doug Hill on 8/15/19.
+//  Copyright © 2019 Doug Hill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
